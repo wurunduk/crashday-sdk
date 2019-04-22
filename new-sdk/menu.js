1170ba638f00db558e6cd7353936ba61f19e57bb
Freed8m <49842407+Freed8m@users.noreply.github.com>
2019-04-22 16:38:42 +0200

    Update menu.js

--- new-sdk/menu.js
+++ new-sdk/menu.js
@@ -29,7 +29,7 @@ function OpenPage( head, title )
       	MenuStr += '  &nbsp;&nbsp;<a href="3_started_modding.html">&gt;  Getting started with modding</a><br>';
 	
       	MenuStr += '  <br><b class="caption">5. Tools Reference</b><br><br>';
-      	MenuStr += '  &nbsp;&nbsp;<a href="3_started_modding.html">&gt;  Blender export plugin</a><br>';
+      	MenuStr += '  &nbsp;&nbsp;<a href="5_tools_blender_export">&gt;  Blender export plugin</a><br>';
 
       	MenuStr += '  <br><br><br>&nbsp;</td>';
         MenuStr += '  <td width=60 height=600>&nbsp;</td>';
